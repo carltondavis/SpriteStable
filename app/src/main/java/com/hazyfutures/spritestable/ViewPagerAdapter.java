3756e5e78d90b9666bb7a9c3d14d75f1a04b50bc
carltondavis <carltondavis@gmail.com>
2015-02-06 13:20:35 -0600

    Made list of Qualities

--- app/src/main/java/com/hazyfutures/spritestable/ViewPagerAdapter.java
+++ app/src/main/java/com/hazyfutures/spritestable/ViewPagerAdapter.java
@@ -1,13 +1,5 @@
 package com.hazyfutures.spritestable;
 //TODO COMPILE FRAGMENT:
-//TODO Something multiplying sprites in list?  Haven't seen recently
-//TODO Test update for each attribute
-
-//Todo Heal after 24 hours consecutive rest
-//Todo karma regen after 8 hours consecutive rest
-//todo test consecutive rest karma reset
-//ToDo Change Hours to Total Hours.
-//ToDo Add penalty popup warnings  Add Total Penalty display, tap it to have a toast pop up listing sources of penalties
 
 //TODO SPRITE FRAGMENT:
 //Todo Add display of sprite stats + abilities for specific sprite listed
@@ -17,15 +9,66 @@ package com.hazyfutures.spritestable;
 //Todo Update Compile/Register/Rest to account for qualities
 
 //TODO QUALITIES FRAGMENT:
-//Todo add Insomnia A/B Checkbox to turn it on, radiobutton to pick which type, add code to check when regen karma, adds counter for failed rolls to keep karma refresh button disabled, add code to muck with healing
+//Todo list of qualities:
+//Todo: Create database to hold these records
+//Todo: UI widgets for each entry
+//Todo: Code to load and save info
+//Todo: Add modifiers for any given die rolls to make these stats generic (MatrixMod, PerceptionMod, HealingMod, etc)
+//Todo: Add code for existing die rolls
+
+//Codeslinger: +2 dice a specific matrix action
+//Focused concentration 1-6: May sustain Rating force of without negative penalties
+//High paint tolerance 1-3: +1 rating boxes of stun/physical before penalties are incurred
+//Home ground: +2 bonus to matrix tests
+//Natural Hardening: +1 biofeedback filtering
+//Quick Healer: +2 dice pool on healing tests
+//Toughness: +1 die damage resistance tests
+//Will to live 1-3: +Rating in overflow boxes
+//Bad Luck: d6 when edge used, on a 1 opposite of effect occurs, only happens once per session
+//Codeblock -2 dice pool on a specific action
+//Loss of Confidence:  -2 dice on a skill
+//Low pain tolerance: Modifier every 2 boxes of damage, not 3
+//Sensitive system: Willpower(2) test before fading tests, +2 fading if it fails
+//Simsense Vertigo: -2 dice all matrix tests
+//Slow Healer: -2 dice on healing tests
+//Perceptive 1-2: +Rating die matrix perception test
+//Spike Resistance 1-3: +rating vs. biofeedback
+//Tough as Nails 1-4 Stun/Physical: +1 box of either stun or physical per rating, up to 3 on either.
+//Asthma: Fatigue damage twice as often, 2 boxes fatigue damage=-1 all actions, 4 boxes=further fatigue damage only resisted by willpower, 8 boxes=additional -1 penatly all actions
+//Bi-polar: Once per day at sleep time roll die, 1-2 Depressed, 3-4 Manic, 5-6Stable, Medication available.  Not taking for 12 hours results in roll
+//Results: Manic: -2 logic + intuition rolls, Depressive -2 logic, intuition, Stable no penalties
+//Blind: -4 perception tests
+//Computer illiterate: -4 all matrix tests
+//Deaf: -2 perception tests
+//Dimmer bulb: -1 logic + intuition tests
+//Illiterate: -2 matrix tests
+//Oblivious 1-2: (1) -2 perception tests, (2) +1 threshold perception tests
+//Pie Iesu Domine: High pain tolerence 1, Always has one box physical damage
+//Noise reduction from equipment
+
+
 //ToDo Add possible Overflow/Death warnings If compiling/registering something, calculate if worst case stun/physical could max out stun or physical chart.  Warn of possibility.
 //ToDo Add automated registering system: Set the amount of time to use, and it runs
 
 //Todo: MATRIX FRAGMENT
-//Todo: Add Playing hacking buttons
-//Todo: Add Playing List of sprites and Sprite assistance/assist sprite radio buttons
+//Todo: Add Noise rating distance spinner
+//Todo: Add Noise rating static spinner
+//Todo: Add Playing hacking buttons, including manual die pool modifier
+//Todo: Design convenient widget for picking sprites to assist or vice versa (Dropdown, Player Action/Sprite Action)
+//Todo: Add code to calculate dice pools for each button including penalties, and
+//Todo: add code to calculate dice pools for assistance rolls
+//Todo: Roll dice button, determine final pool button (assistance), and list manual die rolls
+//Todo: Calculate limits for rolls
+//Todo: Drain for matrix spells
 
 //Fancy UI:
+//TODO Something multiplying sprites in list?  Haven't seen recently
+//TODO Test update for each attribute
+//Todo Heal after 24 hours consecutive rest
+//Todo karma regen after 8 hours consecutive rest
+//todo test consecutive rest karma reset
+//ToDo Change Hours to Total Hours.
+//ToDo Add penalty popup warnings  Add Total Penalty display, tap it to have a toast pop up listing sources of penalties
 //ToDo Add Post-edge buttons for skill and drain. Set minimum number of hits desired for roll, re-roll failures and subtract edge if that number not met. Use Toast if edge used this way
 //Todo add Toast for disabled buttons explaining why they're disabled
 //Todo Someday Add statistics for rolls to include actual percentage chance of something happening to warnings
